dd3947cfe9ae0d859e89fa505f9aa383ab134490
NoFlaw <brentonbates@gmail.com>
2015-03-07 03:42:24 -0500

    Adding Nunit, and referencing JustMock. Adding Unit and Integration testing solutions. Small stub ExampleTest for Integration with small usage and guidance. Adding abstract Specification class to enforce default behavior for UnitTesting with Act and Arrange methods. First unit test done for StudentService which checks GetById method call

---
+++ BrentUniversity_Azure.IntegrationTests/ExampleIntegrationTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Telerik.JustMock;
+using Telerik.JustMock.AutoMock;
+
+namespace BrentUniversity_Azure.IntegrationTests
+{
+    /// <summary>
+    /// A Example Integration Test
+    /// </summary>
+    [TestFixture]
+    [Category("ExampleTests.Integration")]
+    public class when_making_an_example_integration_test
+    {
+        #region Example Options With Explanations - Setup and Tear down
+
+        /// <summary>
+        /// This runs only once at the beginning of all tests and is used for all tests in the
+        /// class.
+        /// </summary>
+        //[TestFixtureSetUp]
+        //public void InitialSetup()
+        //{
+        //}
+
+        /// <summary>
+        /// This runs only once at the end of all tests and is used for all tests in the class.
+        /// </summary>
+        //[TestFixtureTearDown]
+        //public void FinalTearDown()
+        //{
+        //}
+
+        /// <summary>
+        /// This setup funcitons runs before each test method
+        /// </summary>
+        //[SetUp]
+        //public void SetupForEachTest()
+        //{
+        //}
+
+        /// <summary>
+        /// This setup funcitons runs after each test method
+        /// </summary>
+        //[TearDown]
+        //public void TearDownForEachTest()
+        //{
+        //}
+
+        #endregion
+        
+        //You would declare your variables here
+        //private Person _person;
+            
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            //Todo: Uncomment when using Automapper library
+            //AutoMapperConfiguration.Configure();
+
+            //Todo: New up objects and call your services and/or create repos and set private variables
+            //var model = new SomeClass();
+            //_repository = new Repository<DbObject>(model);
+            //var service = new Service(_repository);
+            //_person = service.FindById(1);
+        }
+
+        
+        [Test]
+        public void this_is_a_fake_test()
+        {
+            //Todo: Check for did test pass
+            //Assert.IsNotNull(_person);
+        }
+
+        
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            //Todo: Delete whatever you did (example: records saved in the database, when created in the Integration Test)
+            //_repository.Delete(_person);
+            //_repository.SaveChanges();
+        }
+    }
+}
---
+++ BrentUniversity_Azure.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BrentUniversity_Azure.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Telerik AD")]
+[assembly: AssemblyProduct("BrentUniversity_Azure.UnitTests")]
+[assembly: AssemblyCopyright("Copyright @ Telerik 2002-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ca9a1b6f-7d30-4960-a98d-d1a12d973aac")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("123.4.5.0")]
+[assembly: AssemblyFileVersion("123.4.5.0")]
