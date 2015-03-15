b78f2d7198df0777304f6e4ffba647424dc0456c
NoFlaw <brentonbates@gmail.com>
2015-03-15 03:32:56 -0400

    Adding ability to view and download pdf copy of resume, added a helper class for getting a file by path and returning bytes, displayed within list upon Profile page

--- BrentUniversity_Azure/Controllers/HomeController.cs
+++ BrentUniversity_Azure/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using System.IO;
+using System.Web.Mvc;
+using BrentUniversity_Azure.Helper;
 
 namespace BrentUniversity_Azure.Controllers
 {
@@ -18,5 +20,26 @@ namespace BrentUniversity_Azure.Controllers
         {
             return View();
         }
+
+        public FileResult ViewPdf()
+        {
+            var filePath = Server.MapPath("~/Content/resume/BrentonBates_WebDev_Resume.pdf");
+            var pdfFileBytes = FileHelper.GetBytesFromFile(filePath);
+            //Forcing into MemoryStream in order to clear response and add inline header for viewing in new tab
+            var stream = new MemoryStream();
+            stream.Write(pdfFileBytes, 0, pdfFileBytes.Length);
+            stream.Position = 0;
+            HttpContext.Response.Clear();
+            HttpContext.Response.AddHeader("content-disposition", "inline; filename=Brenton Bates Business Application Developer.pdf");
+            return File(stream, "application/pdf");
+        }
+
+        public FileResult DownloadPdf()
+        {
+            var filePath = Server.MapPath("~/Content/resume/BrentonBates_WebDev_Resume.pdf");
+            var pdfFileBytes = FileHelper.GetBytesFromFile(filePath);
+            return File(pdfFileBytes, "application/pdf", "Brenton Bates Business Application Developer.pdf");
+        }
+
     }
 }
