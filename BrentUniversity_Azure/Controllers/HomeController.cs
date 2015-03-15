325dff7cdbe7a9b19a4cbf5d6db357304ca57871
NoFlaw <brentonbates@gmail.com>
2015-03-15 13:00:30 -0400

    Edits to make viewing resume a plain html5 with .html extension; removing actionlink to controller call, and used anchor tag; removed fileresult action method from HomeController

--- BrentUniversity_Azure/Controllers/HomeController.cs
+++ BrentUniversity_Azure/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BrentUniversity_Azure.Helper;
 
 namespace BrentUniversity_Azure.Controllers
@@ -19,19 +18,6 @@ namespace BrentUniversity_Azure.Controllers
         public ActionResult MyProfile()
         {
             return View();
-        }
-
-        public FileResult ViewPdf()
-        {
-            var filePath = Server.MapPath("~/Content/resume/BrentonBates_WebDev_Resume.pdf");
-            var pdfFileBytes = FileHelper.GetBytesFromFile(filePath);
-            //Forcing into MemoryStream in order to clear response and add inline header for viewing in new tab
-            var stream = new MemoryStream();
-            stream.Write(pdfFileBytes, 0, pdfFileBytes.Length);
-            stream.Position = 0;
-            HttpContext.Response.Clear();
-            HttpContext.Response.AddHeader("content-disposition", "inline; filename=Brenton Bates Business Application Developer.pdf");
-            return File(stream, "application/pdf");
         }
 
         public FileResult DownloadPdf()
